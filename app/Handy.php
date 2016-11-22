e729794684c9e974f459a4f0adc8d858450a00db
Nils Mundhenke (@rabalyn) <n.mundhenke@mailbox.org>
2016-11-22 18:04:21 +0100

    Initial Handy class

---
+++ app/Handy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+class Handy {
+    private $hersteller;
+    private $modell;
+    private $preis;
+
+    public function toString() {
+        return $this->modell . ' von ' . $this->hersteller . ' zum Preis von ' . $this->preis;
+    }
+
+    public function getHersteller() {
+        return $this->hersteller;
+    }
+
+    public function getModell() {
+        return $this->modell;
+    }
+
+    public function getPreis() {
+        return $this->preis;
+    }
+
+    public function setHersteller($hersteller) {
+        $this->hersteller = $hersteller;
+    }
+
+    public function setModell($modell) {
+        $this->modell = $modell;
+    }
+
+    public function setPreis($preis) {
+        $this->preis = $preis;
+    }
+}
